a04834b20bc3889c8a014bf482e1a776abacaafa
Roman De Santos <46465589+roman-de-santos@users.noreply.github.com>
2025-11-01 18:08:12 -0700

    Lab3

---
+++ Lab2/fpga/Radiant/Sim/Sim.f
@@ -0,0 +1,27 @@
+-L work
+-reflib pmi_work
+-reflib ovi_ice40up
+
+
+"C:/Users/rdesantos/Downloads/Lab2/source/impl_1/SegDisp.sv" 
+"C:/Users/rdesantos/Downloads/Lab2/source/impl_1/top.sv" 
+"C:/Users/rdesantos/Downloads/Lab2/source/impl_1/ClockDiv.sv" 
+"C:/Users/rdesantos/Downloads/Lab2/source/impl_1/top_tb.sv" 
+-sv
+-optionset VOPTDEBUG
++noacc+pmi_work.*
++noacc+ovi_ice40up.*
+
+-vopt.options
+  -suppress vopt-7033
+-end
+
+-gui
+-top top_tb
+-vsim.options
+  -suppress vsim-7033,vsim-8630,3009,3389
+-end
+
+-do "view wave"
+-do "add wave /*"
+-do "run -all"
